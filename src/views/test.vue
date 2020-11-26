d8c683075c2ccefb12d88d141c8e5aa5d702e049
unknown <317607692@qq.com>
2020-11-26 10:02:03 +0800

    测试页面正在测试el表单验证

--- src/views/test.vue
+++ src/views/test.vue
@@ -1,11 +1,108 @@
 <template>
-  <div>
-    <div>fuck</div>
-  </div>
+    <div>
+        <el-form ref="form" :model="form" label-width="80px" :rules="rules">
+            <el-form-item prop="name" label="姓名">
+                <el-input
+                    v-model="form.name"
+                    placeholder="请输入内容"
+                    clearable
+                ></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="submitForm('form')">Submit</el-button>
+            </el-form-item>
+        </el-form>
+    </div>
 </template>
 
 <script>
-export default {}
+export default {
+    data() {
+        return {
+            form: {
+                name: "",
+                region: "",
+                date1: "",
+                date2: "",
+                delivery: false,
+                type: [],
+                resource: "",
+                desc: "",
+            },
+            rules: {
+                name: [
+                    {
+                        required: true,
+                        message: "请输入活动名称",
+                        trigger: "blur",
+                    },
+                    {
+                        min: 3,
+                        max: 20,
+                        message: "长度在 3 到 20 个字符",
+                        trigger: "blur",
+                    },
+                ],
+                region: [
+                    {
+                        required: true,
+                        message: "请选择活动区域",
+                        trigger: "change",
+                    },
+                ],
+                date1: [
+                    {
+                        type: "date",
+                        required: true,
+                        message: "请选择日期",
+                        trigger: "change",
+                    },
+                ],
+                date2: [
+                    {
+                        type: "date",
+                        required: true,
+                        message: "请选择时间",
+                        trigger: "change",
+                    },
+                ],
+                type: [
+                    {
+                        type: "array",
+                        required: true,
+                        message: "请至少选择一个活动性质",
+                        trigger: "change",
+                    },
+                ],
+                resource: [
+                    {
+                        required: true,
+                        message: "请选择活动资源",
+                        trigger: "change",
+                    },
+                ],
+                desc: [
+                    {
+                        required: true,
+                        message: "请填写活动形式",
+                        trigger: "blur",
+                    },
+                ],
+            },
+        };
+    },
+    methods: {
+        submitForm(formName){
+            this.$refs[formName].validate((valid) => {
+                if(valid){
+                    alert("submit");
+                }else{
+                    alert("error");
+                }
+            });
+        }
+    },
+};
 </script>
 <style scoped>
 </style>
